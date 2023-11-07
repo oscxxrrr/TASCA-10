74022acf9cadb0b2264de9b1569b0ad9a6aad71b
oscxxrrr <omartinez20714@iesjoanramis.org>
2023-11-07 13:55:43 +0100

    pujar 11

--- Ex11.py
+++ Ex11.py
@@ -18,71 +18,22 @@ def calculadora_enteros():
         4.Dividir
         5.Salir
           """)
-        op = int(input("Elige una opcion: "))
-        match op:
-            case 1: #Sumar
-                x = int(input("Introduze el primer numero: "))
-                y = int(input("Introduze el segundo numero: "))
-                print("{} + {} = {}".format(x, y, x+y))
-            case 2: #Restar
-                x = int(input("Introduze el primer numero: "))
-                y = int(input("Introduze el segundo numero: "))
-                print("{} - {} = {}".format(x, y, x-y))
-            case 3: #Multiplicar
-                x = int(input("Introduze el primer numero: "))
-                y = int(input("Introduze el segundo numero: "))
-                print("{} * {} = {}".format(x, y, x*y))
-            case 4: #Dividir
-                x = int(input("Introduze el primer numero: "))
-                y = int(input("Introduze el segundo numero: "))
-                print("{} / {} = {}".format(x, y, x/y))
-            case 5: #Salir
-                print("Has salido de la calculadora de numeros enteros")
-                op = -1
+    eleccion = int(input())
+    if eleccion == 1:
+        print("Resultado: ", numero1, "+", numero2, "=", numero1+numero2)
+    
+    if eleccion == 2:
+        print("Resultado: ", numero1, "-", numero2, "=", numero1-numero2)
 
-def calculadora_reales():
-    op = 1 
-    while op>0:
-        print("""
-        Menu Reales:
-        1.Sumar
-        2.Restar
-        3.Multiplicar 
-        4.Dividir
-        5.Salir
-          """)
-        op = int(input("Elige una opcion: "))
-        match op:
-            case 1: #Sumar
-                x = float(input("Introduze el primer numero: "))
-                y = float(input("Introduze el segundo numero: "))
-                print("{} + {} = {}".format(x, y, x+y))
-            case 2: #Restar
-                x = float(input("Introduze el primer numero: "))
-                y = float(input("Introduze el segundo numero: "))
-                print("{} - {} = {}".format(x, y, x-y))
-            case 3: #Multiplicar
-                x = float(input("Introduze el primer numero: "))
-                y = float(input("Introduze el segundo numero: "))
-                print("{} * {} = {}".format(x, y, x*y))
-            case 4: #Dividir
-                x = float(input("Introduze el primer numero: "))
-                y = float(input("Introduze el segundo numero: "))
-                print("{} / {} = {}".format(x, y, x/y))
-            case 5: #Salir
-                print("Has salido de la calculadora de numeros reales")
-                op = -1
-       
-#Programa principal
-a = 1
-while a>0:
-    a = menu_principal()
-    match a:
-        case 1:
-            calculadora_enteros()
-        case 2:
-            calculadora_reales()
-        case 3:
-            a = -1
-        case other:
-            print("Opcion no valida")
+    if eleccion == 3:
+        print("Resultado: ", numero1, "*", numero2, "=", numero1*numero2)
+    
+    if eleccion == 4:
+        print("Resultado: ", numero1, "/", numero2, "=", numero1/numero2)
+    
+    if eleccion == 5:
+       numero1=int(input("Ingresa un número: "))
+       numero2=int(input("Ingresa otro número: "))
+
+    if eleccion == 6:
+        print("Has salido de la calculadora creada por: Óscar Martínez")
