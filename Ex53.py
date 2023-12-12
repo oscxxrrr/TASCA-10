f717788da6b7033c123957c516f0acc86ffb354e
oscxxrrr <omartinez20714@iesjoanramis.org>
2023-12-12 12:44:40 +0100

    sube todos

---
+++ Ex53.py
@@ -0,0 +1,16 @@
+def parells():
+    a = []
+    for i in range(2, 101, 2):
+        a.append(i)
+    print("Els parells d'1 a 100 són {}".format(a))
+
+def senars():
+    a = []
+    for i in range(1, 100, 2):
+        a.append(i)
+    print("Els senars d'1 a 100 són {}".format(a))
+
+# Programa principal
+parells()
+senars()
+
